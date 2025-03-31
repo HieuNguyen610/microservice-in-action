30ae6cf2366a640b9288c0c8758de4ce1332a527
hieunm2 <hieunm2@vnext.vn>
2025-03-31 08:55:39 +0700

    init commit

---
+++ src/main/java/hieu/microserviceinaction/MicroserviceInActionApplication.java
@@ -0,0 +1,13 @@
+package hieu.microserviceinaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceInActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceInActionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hieu/microserviceinaction/MicroserviceInActionApplicationTests.java
@@ -0,0 +1,13 @@
+package hieu.microserviceinaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceInActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
