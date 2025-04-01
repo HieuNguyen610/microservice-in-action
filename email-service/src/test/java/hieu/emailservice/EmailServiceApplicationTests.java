06bf009ce60ee7596723eb3e0b601ea17372508e
hieunm2 <hieunm2@vnext.vn>
2025-04-01 09:03:31 +0700

    create base-service, order-service, stock-service, email-service

---
+++ email-service/src/test/java/hieu/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package hieu.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
